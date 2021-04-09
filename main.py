e23a61c08e5bf1ab1988f4abf6dba2b51cd63349
Soal <keithsoal@gmail.com>
2021-04-09 08:57:13 +0200

    Integration of strain and temperature classes

--- main.py
+++ main.py
@@ -2,12 +2,23 @@
 
 from getStrain import getStrain
 from getTemp import getTemp
+from time import sleep
 
 # create configuration file
 
+hx = configureStrain()
+device_file = configureTemp()
+
 while True:
     # get data
-    strain = getStrain()
-    temp = getTemp()
-    
+    strain = getStrain(hx)
+    temp = getTemp(device_file)
+    time.sleep(sleeptime)
+
     # write to inlfuxDB
+
+    except (KeyboardInterrupt, SystemExit):
+        print('Bye :)')
+
+        finally:
+            GPIO.cleanup()
